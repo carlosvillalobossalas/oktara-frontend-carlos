f8d3acad46f8b55e28e6c4187344f4ea346d5ff2
cvillalobos2 <cvillalobos@soin.co.cr>
2022-11-05 14:44:32 -0600

    url fixed

--- src/apis/logisticsApi.ts
+++ src/apis/logisticsApi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const logisticsApi = axios.create({
   // baseURL: "http://localhost:8000/api/packages/",
-  baseURL: "https://backend-oktara-carlos.herokuapp.com/api/packages/",
+  baseURL: "https://oktara-carlos.herokuapp.com/api/packages/",
 });
 
 export default logisticsApi;
