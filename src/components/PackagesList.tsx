ea8de067cb0e10aad1981b3369a475c4b84fc9f6
cvillalobos2 <cvillalobos@soin.co.cr>
2022-11-05 23:41:50 -0600

    filter packages added

--- src/components/PackagesList.tsx
+++ src/components/PackagesList.tsx
@@ -2,16 +2,29 @@ import { logisticsApi } from "../apis";
 import { Package } from "../interfaces/packages";
 import { PackageContext } from "../context/packages/PackageContext";
 import { PackageInfoCard } from "./PackageInfoCard";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
 
 export const PackagesList = () => {
   const { refresh, packages, setPackages } = useContext(PackageContext);
+  const [filterPackages, setFilterPackages] = useState("all");
 
   const getAllPackages = async () => {
     const response = await logisticsApi.get("/");
     if (response.status === 200) {
-      setPackages(response.data.packages);
+      let packagesResponse: Package[] = response.data.packages;
+      let packagesSorted = packagesResponse.sort((a, b) =>
+        b.status < a.status ? 1 : b.status > a.status ? -1 : 0
+      );
+      setPackages(packagesSorted);
     }
+  };
+
+  const handleToggleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newFilter: string
+  ) => {
+    setFilterPackages(newFilter);
   };
 
   useEffect(() => {
@@ -21,10 +34,26 @@ export const PackagesList = () => {
   return (
     <div className="package-list-container">
       <h4>Packages</h4>
+      <ToggleButtonGroup
+        color="primary"
+        value={filterPackages}
+        exclusive
+        onChange={handleToggleChange}
+        aria-label="Platform"
+      >
+        <ToggleButton value="Pending">Pending</ToggleButton>
+        <ToggleButton value="all">All</ToggleButton>
+        <ToggleButton value="Shipped">Shipped</ToggleButton>
+      </ToggleButtonGroup>
       <div className="package-list">
-        {packages?.map((p: Package) => (
-          <PackageInfoCard key={p.id} packageInfo={p} />
-        ))}
+        {packages
+          ?.filter((p) => {
+            if (filterPackages === "all") return true;
+            else return p.status === filterPackages;
+          })
+          .map((p: Package) => (
+            <PackageInfoCard key={p.id} packageInfo={p} />
+          ))}
       </div>
     </div>
   );
